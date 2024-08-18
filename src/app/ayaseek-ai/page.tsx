672581cc0d28b2d7c97ce7a47def173e4be06c45
dadanisme <dadanisme@upi.edu>
2024-08-18 19:26:49 +0700

    update app

--- src/app/ayaseek-ai/page.tsx
+++ src/app/ayaseek-ai/page.tsx
@@ -12,7 +12,7 @@ export default function NotFound() {
         <h1 className="text-4xl text-secondary font-bold">Download</h1>
         <p className="text-lg">ayaseek-ai-preview.apk</p>
         <a
-          href="https://expo.dev/artifacts/eas/vQrJESZpS5k7ErXTSs3jx8.apk"
+          href="https://expo.dev/artifacts/eas/YAznyqcMskNApxZ9iFPUR.apk"
           download="ayaseek-ai.apk"
           target="_blank"
           className="mt-4 btn btn-secondary btn-sm"
